7e449ba6275dfb09c81ba2cfd996f14d268187f9
charlestsai1995 <caicanyu1995@gmail.com>
2016-09-16 13:39:40 +0800

    adding config

---
+++ netease.pac
@@ -0,0 +1,5 @@
+function FindProxyForURL(url, host) {
+   if (host == 'music.163.com') return 'PROXY 183.61.236.54:3128';
+   return 'DIRECT';
+}
+//2016-01-21 15:31:52
---
+++ request.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import requests
+import xml.dom.minidom
+import sys
+import time
+import linecache
+
+f = open('ip.xml','w')
+r = requests.get('http://www.getproxy.jp/proxyapi?ApiKey=805b81a32fa3088eaf73dad1f3ec61f6912a6d05&area=CN&sort=requesttime&orderby=asc&page=5')
+r=str(r.text)
+f.writelines(r)
+f.close()
+dom = xml.dom.minidom.parse('ip.xml')
+ip=dom.getElementsByTagName('ip')
+pingt=dom.getElementsByTagName('requesttime')
+test=pingt[len(pingt)-1]
+test.firstChild.data=int(test.firstChild.data)
+if test.firstChild.data<2:
+	exit()
+bl=open('/home/test/blacklist.txt','r')
+blist=bl.read()
+fit = 0
+i=0
+for index, element in enumerate(ip):
+	pingt[index] = int(pingt[index].firstChild.data)
+	if pingt[index] < 1 or pingt[index] > 2000:
+		print 'ping not pass ' + element.firstChild.data
+	else:
+		print 'ping pass ' + element.firstChild.data
+		if element.firstChild.data in blist:
+			print 'excluded'
+		else:
+			fit = 1
+			i = index
+			break
+if fit == 1:
+	print 'result ' + ip[i].firstChild.data
+	currentip_file=open('current.txt','w+')
+	currentip_file.write(ip[i].firstChild.data)
+	pac=open('/var/www/html/netease.pac','r+')
+	flist=pac.readlines()
+	flist[1]='   if (host == \'music.163.com\') return \'PROXY '+ip[i].firstChild.data+'\';\n'
+	flist[2]='   if (shExpMatch(url,\"*.xiami.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[3]='   if (shExpMatch(url,\"*.xiami.net/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[4]='   if (shExpMatch(url,\"*.cnzz.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[5]='   if (shExpMatch(url,\"*.tudou.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	flist[8]='//'+localtime+'\n'
+	pac=open('/var/www/html/netease.pac','w+')
+	pac.writelines(flist)
+	pac.close()
+	webpage=open('/var/www/html/index.htm','r+')
+	pagecon=webpage.readlines()
+	pagecon[6]='<center>Current server: ' + ip[i].firstChild.data +'</center>\n'
+	pagecon[7]='<center>Update time: ' + localtime +'(EST)</center>\n'
+	webpage=open('/var/www/html/index.htm','w+')
+	webpage.writelines(pagecon)
+	webpage.close()
+else:
+	print 'no fit'

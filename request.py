d56cb40478ce8d71034a7ea4dea81d2aa1939eee
charlestsai1995 <caicanyu1995@gmail.com>
2016-09-17 14:55:10 +0800

    fix path 2

--- request.py
+++ request.py
@@ -9,8 +9,8 @@ import os
 
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
 conf = ConfigParser.ConfigParser()
-conf.read(path+"config.ini")
-f = open(path+'ip.xml','w')
+conf.read(path+'/config.ini')
+f = open(path+'/ip.xml','w')
 r = requests.get('http://www.getproxy.jp/proxyapi?ApiKey='+conf.get('api','apikey')+'&area=CN&sort=requesttime&orderby=asc&page='+conf.get('api','page'))
 r=str(r.text)
 f.writelines(r)
@@ -22,7 +22,7 @@ test=pingt[len(pingt)-1]
 test.firstChild.data=int(test.firstChild.data)
 if test.firstChild.data<2:
 	exit()
-bl=open(path+'blacklist.txt','r')
+bl=open(path+'/blacklist.txt','r')
 blist=bl.read()
 fit = 0
 i=0
@@ -40,25 +40,25 @@ for index, element in enumerate(ip):
 			break
 if fit == 1:
 	print 'result ' + ip[i].firstChild.data
-	currentip_file=open(path+'current.txt','w+')
+	currentip_file=open(path+'/current.txt','w+')
 	currentip_file.write(ip[i].firstChild.data)
-	pac=open(conf.get('path','web')+'netease.pac','r+')
+	pac=open(conf.get('path','web')+'/netease.pac','r+')
 	flist=pac.readlines()
 	flist[1]='   if (host == \'music.163.com\') return \'PROXY '+ip[i].firstChild.data+'\';\n'
-	flist[2]='   if (shExpMatch(url,\"*.xiami.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
-	flist[3]='   if (shExpMatch(url,\"*.xiami.net/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
-	flist[4]='   if (shExpMatch(url,\"*.cnzz.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
-	flist[5]='   if (shExpMatch(url,\"*.tudou.com/*\")) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[2]='   if (shExpMatch(url,\'*.xiami.com/*\')) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[3]='   if (shExpMatch(url,\'*.xiami.net/*\')) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[4]='   if (shExpMatch(url,\'*.cnzz.com/*\')) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
+	flist[5]='   if (shExpMatch(url,\'*.tudou.com/*\')) return \'PROXY ' +ip[i].firstChild.data+'\';\n'
 	localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 	flist[8]='//'+localtime+'\n'
-	pac=open(conf.get('path','web')+'netease.pac','w+')
+	pac=open(conf.get('path','web')+'/netease.pac','w+')
 	pac.writelines(flist)
 	pac.close()
-	webpage=open(conf.get('path','web')+'index.htm','r+')
+	webpage=open(conf.get('path','web')+'/index.htm','r+')
 	pagecon=webpage.readlines()
 	pagecon[6]='<center>Current server: ' + ip[i].firstChild.data +'</center>\n'
 	pagecon[7]='<center>Update time: ' + localtime +'(EST)</center>\n'
-	webpage=open(conf.get('path','web')+'index.htm','w+')
+	webpage=open(conf.get('path','web')+'/index.htm','w+')
 	webpage.writelines(pagecon)
 	webpage.close()
 else:
